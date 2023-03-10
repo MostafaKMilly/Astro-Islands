4545b5a1d0a65536c35a42f654c0d665c7a1b811
Mostafa Milly <mostafamilly6@gmail.com>
2023-03-10 23:45:27 +0200

    Install frameworks intergrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import solidJs from "@astrojs/solid-js";
+import vue from "@astrojs/vue";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), solidJs(), vue(), svelte()]
+});
